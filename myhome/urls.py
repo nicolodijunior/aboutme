6881fb21dd4899b445917bd81eb655a33c519455
Junior Nicolodi <jnicolodijunior@gmail.com>
2023-05-25 09:34:30 -0300

    Post Page like correceted, missing comments

--- myhome/urls.py
+++ myhome/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("blog/all/update_post/<int:post_id>", views.update_post, name="update_post"),
     path("blog/all/comment/<int:post_id>", views.comment, name="comment"),
     path("post", views.post, name="post"),
+    path("cv", views.loadcv, name="loadcv"),
 ]
